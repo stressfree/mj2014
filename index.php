c35d4869bf6d5626880855fb3670fe12cb65e7ec
David Harrison <david.harrison@stress-free.co.nz>
2014-05-22 11:41:19 +1200

    Added the Google Analytics tracking details.

--- index.php
+++ index.php
@@ -46,5 +46,13 @@ xhtml1/DTD/xhtml1-transitional.dtd">
             </div>
         </div>
         <script type="text/javascript" src="/templates/mj2014/js/script.js"></script>
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+          ga('create', 'UA-27259702-2', 'martinjenkins.co.nz');
+          ga('send', 'pageview');
+        </script>
     </body>
 </html>
